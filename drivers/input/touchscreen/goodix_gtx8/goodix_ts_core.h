0e9fa81e452bbfddcbe69bca61bc07c1d752ff25
UtsavisGreat <utsavbalar1231@gmail.com>
2019-07-07 19:02:28 -0400

    goodix_driver_gtx8: switch to qcom drm notifier
    * also switch fb notifier api to qcom drm notifier
    
    Change-Id: If51affe8627ef3f9ed8aaabace12192e4c1d4f1f
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: HarukeyUA <inazarrusnak@gmail.com>

--- drivers/input/touchscreen/goodix_gtx8/goodix_ts_core.h
+++ drivers/input/touchscreen/goodix_gtx8/goodix_ts_core.h
@@ -47,6 +47,7 @@
 #include <linux/earlysuspend.h>
 #endif
 #ifdef CONFIG_FB
+#include <linux/msm_drm_notify.h>
 #include <linux/notifier.h>
 #include <linux/fb.h>
 #endif
@@ -399,7 +400,7 @@ struct goodix_ts_esd {
  * @suspended: suspend/resume flag
  * @ts_notifier: generic notifier
  * @ts_esd: esd protector structure
- * @fb_notifier: framebuffer notifier
+ * @msm_drm_notifier: framebuffer notifier
  * @early_suspend: early suspend
  */
 struct goodix_ts_core {
@@ -432,8 +433,8 @@ struct goodix_ts_core {
 	struct goodix_ts_esd ts_esd;
 
 #ifdef CONFIG_FB
-	struct notifier_block fb_notifier;
-	struct work_struct fb_notify_work;
+	struct notifier_block msm_drm_notifier;
+	struct work_struct msm_drm_notify_work;
 #elif defined(CONFIG_HAS_EARLYSUSPEND)
 	struct early_suspend early_suspend;
 #endif
@@ -724,7 +725,7 @@ int goodix_ts_register_notifier(struct notifier_block *nb);
 int goodix_generic_noti_callback(struct notifier_block *self,
 				unsigned long action, void *data);
 
-int goodix_ts_fb_notifier_callback(struct notifier_block *self,
+int goodix_ts_msm_drm_notifier_callback(struct notifier_block *self,
 			unsigned long event, void *data);
 
 extern void goodix_msg_printf(const char *fmt, ...);
