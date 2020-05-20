cf84d74266b0ee766b68d39c58d4da771aa1ee78
Demon000 <demonsingur@gmail.com>
2020-05-21 01:37:19 +0300

    drm: msm: sde: Translate zpos property to fod property
    Squashed with:
    Author: GuaiYiHu <guaiyihu@foxmail.com>
    Date:   Thu Aug 13 12:25:09 2020 +0800
    
        drm: sde: Increase sde stage to map zpos changes
    
         * Another stage should be added, otherwise fingerprint payment won't work in some apps
         like Alipay and Jingdong.
    
        Change-Id: Ibbd928b6d75c17f9c6c2e8b6f72b2d17aa48eb94
    
    Author: Pig <pig.priv@gmail.com>
    Date:   Fri Aug 14 20:06:43 2020 +0800
    
        uapi: sde_drm: Define FOD_PRESSED_LAYER_ZORDER
    
        Change-Id: I5b25d19fab3a2901948d639c785165e70786f4ab
    
    Change-Id: I7e8562ed3eda675d160d6dabee7e568a1a72e849

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -108,6 +108,9 @@ enum msm_mdp_plane_property {
 
 	/* range properties */
 	PLANE_PROP_ZPOS = PLANE_PROP_BLOBCOUNT,
+#ifdef CONFIG_MACH_XIAOMI_F9S
+	PLANE_PROP_FOD,
+#endif
 	PLANE_PROP_ALPHA,
 	PLANE_PROP_COLOR_FILL,
 	PLANE_PROP_H_DECIMATE,
