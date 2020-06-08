ff0fdba60696545c42d345fd97622b57d5915b18
Demon000 <demonsingur@gmail.com>
2020-06-08 17:16:57 +0200

    drm: msm: dsi-staging: cleanup notifier implementation
    Change-Id: If60a789b666cd2c5ab767406d2aeabb03f38ba5b
    (cherry-picked from commit e2be2d5e63433ca5fd89b13865eee1d23fab4147)
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -1691,7 +1691,6 @@ int dsi_display_set_power(struct drm_connector *connector,
 	struct dsi_display *display = disp;
 #ifdef CONFIG_MACH_XIAOMI_F9S
 	struct msm_drm_notifier notify_data;
-	int event = power_mode;
 #endif
 	int rc = 0;
 	struct drm_device *dev = NULL;
@@ -1728,9 +1727,9 @@ int dsi_display_set_power(struct drm_connector *connector,
 	}
 #endif
 
-
 #ifdef CONFIG_MACH_XIAOMI_F9S
-	notify_data.data = &event;
+	notify_data.data = &power_mode;
+	notify_data.id = MSM_DRM_PRIMARY_DISPLAY;
 #endif
 
 	switch (power_mode) {
