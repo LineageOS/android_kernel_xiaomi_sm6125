610f3079fab4e2f0b803c3e0a3edd87cf108b523
Demon000 <demonsingur@gmail.com>
2023-06-05 19:05:19 +0300

    drm: msm: atomic: do not trigger drm notifier
    Change-Id: Ie5c3c44ae12a04957ec43ce4d8b8beb9bad081c3

--- drivers/gpu/drm/msm/msm_atomic.c
+++ drivers/gpu/drm/msm/msm_atomic.c
@@ -225,8 +225,12 @@ msm_disable_outputs(struct drm_device *dev, struct drm_atomic_state *old_state)
 	struct drm_connector_state *old_conn_state;
 	struct drm_crtc *crtc;
 	struct drm_crtc_state *old_crtc_state;
+#ifndef CONFIG_MACH_XIAOMI_F9S
 	struct msm_drm_notifier notifier_data;
 	int i, blank;
+#else
+    int i;
+#endif
 
 	SDE_ATRACE_BEGIN("msm_disable");
 	for_each_connector_in_state(old_state, connector, old_conn_state, i) {
@@ -266,16 +270,16 @@ msm_disable_outputs(struct drm_device *dev, struct drm_atomic_state *old_state)
 		DRM_DEBUG_ATOMIC("disabling [ENCODER:%d:%s]\n",
 				 encoder->base.id, encoder->name);
 
+#ifndef CONFIG_MACH_XIAOMI_F9S
 		if (connector->state->crtc &&
 			connector->state->crtc->state->active_changed) {
 			blank = MSM_DRM_BLANK_POWERDOWN;
 			notifier_data.data = &blank;
 			notifier_data.id = crtc_idx;
-#ifndef CONFIG_MACH_XIAOMI_F9S
             msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK,
 						     &notifier_data);
-#endif
 		}
+#endif
 		/*
 		 * Each encoder has at most one connector (since we always steal
 		 * it away), so we won't call disable hooks twice.
@@ -291,12 +295,14 @@ msm_disable_outputs(struct drm_device *dev, struct drm_atomic_state *old_state)
 			funcs->dpms(encoder, DRM_MODE_DPMS_OFF);
 
 		drm_bridge_post_disable(encoder->bridge);
+#ifndef CONFIG_MACH_XIAOMI_F9S
 		if (connector->state->crtc &&
 			connector->state->crtc->state->active_changed) {
 			DRM_DEBUG_ATOMIC("Notify blank\n");
 			msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK,
 						&notifier_data);
 		}
+#endif
 	}
 
 	for_each_crtc_in_state(old_state, crtc, old_crtc_state, i) {
@@ -441,8 +447,12 @@ static void msm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
 	int bridge_enable_count = 0;
+#ifndef CONFIG_MACH_XIAOMI_F9S
 	int i, blank;
 	bool splash = false;
+#else
+	int i;
+#endif
 
 	SDE_ATRACE_BEGIN("msm_enable");
 	for_each_oldnew_crtc_in_state(old_state, crtc, old_crtc_state,
@@ -502,6 +512,7 @@ static void msm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 		DRM_DEBUG_ATOMIC("enabling [ENCODER:%d:%s]\n",
 				 encoder->base.id, encoder->name);
 
+#ifndef CONFIG_MACH_XIAOMI_F9S
 		if (kms && kms->funcs && kms->funcs->check_for_splash)
 			splash = kms->funcs->check_for_splash(kms);
 
@@ -512,11 +523,10 @@ static void msm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 			notifier_data.id =
 				connector->state->crtc->index;
 			DRM_DEBUG_ATOMIC("Notify early unblank\n");
-#ifndef CONFIG_MACH_XIAOMI_F9S
             msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK,
 					            &notifier_data);
-#endif
 		}
+#endif
 		/*
 		 * Each encoder has at most one connector (since we always steal
 		 * it away), so we won't call enable hooks twice.
@@ -565,13 +575,14 @@ static void msm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 				 encoder->base.id, encoder->name);
 
 		drm_bridge_enable(encoder->bridge);
-
+#ifndef CONFIG_MACH_XIAOMI_F9S
 		if (splash || (connector->state->crtc &&
 			connector->state->crtc->state->active_changed)) {
 			DRM_DEBUG_ATOMIC("Notify unblank\n");
 			msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK,
 					    &notifier_data);
-		}
+        }
+#endif
 	}
 	SDE_ATRACE_END("msm_enable");
 }
