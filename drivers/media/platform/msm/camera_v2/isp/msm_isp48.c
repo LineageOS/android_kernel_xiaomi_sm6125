tzuyu-xd/android_kernel_xiaomi_ginkgo-2

drivers: camera_v2: Import Xiaomi changes
* Import from ginkgo-q-oss
* Drop virtual camera

Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

drivers/media/platform/msm/camera_v2/isp/msm_isp48.c
@@ -30,7 +30,11 @@
 #define MSM_VFE48_BUS_CLIENT_INIT 0xABAB
 #define VFE48_STATS_BURST_LEN 3
 #define VFE48_UB_SIZE_VFE 2048 /* 2048 * 256 bits = 64KB */
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+#define VFE48_UB_STATS_SIZE 608
+#else
 #define VFE48_UB_STATS_SIZE 352
+#endif
 #define MSM_ISP48_TOTAL_IMAGE_UB_VFE (VFE48_UB_SIZE_VFE - VFE48_UB_STATS_SIZE)
 
 
@@ -319,8 +323,23 @@ static void msm_vfe48_set_bus_err_ign_mask(struct vfe_device *vfe_dev,
 void msm_vfe48_stats_cfg_ub(struct vfe_device *vfe_dev)
 {
 	int i;
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	uint32_t ub_offset = 0, stats_burst_len = 0;
+#else
 	uint32_t ub_offset = 0, stats_burst_len;
+#endif
 	uint32_t ub_size[VFE47_NUM_STATS_TYPE] = {
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+		80, /* MSM_ISP_STATS_HDR_BE */
+		64, /* MSM_ISP_STATS_BG */
+		64, /* MSM_ISP_STATS_BF */
+		64, /* MSM_ISP_STATS_HDR_BHIST */
+		64, /* MSM_ISP_STATS_RS */
+		64, /* MSM_ISP_STATS_CS */
+		64, /* MSM_ISP_STATS_IHIST */
+		64, /* MSM_ISP_STATS_BHIST */
+		80, /* MSM_ISP_STATS_AEC_BG */
+#else
 		64, /* MSM_ISP_STATS_HDR_BE */
 		64, /* MSM_ISP_STATS_BG */
 		32, /* MSM_ISP_STATS_BF */
@@ -330,6 +349,7 @@ void msm_vfe48_stats_cfg_ub(struct vfe_device *vfe_dev)
 		32, /* MSM_ISP_STATS_IHIST */
 		32, /* MSM_ISP_STATS_BHIST */
 		32, /* MSM_ISP_STATS_AEC_BG */
+#endif
 	};
 
 	stats_burst_len = VFE48_STATS_BURST_LEN;

drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -844,6 +844,10 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 		a_ctrl->park_lens.max_step = a_ctrl->max_code_size;
 
 	next_lens_pos = a_ctrl->step_position_table[a_ctrl->curr_step_pos];
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	if (next_lens_pos > 100)
+		next_lens_pos = 100;
+#endif
 	while (next_lens_pos) {
 		/* conditions which help to reduce park lens time */
 		if (next_lens_pos > (a_ctrl->park_lens.max_step *
@@ -867,6 +871,11 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 				(next_lens_pos - a_ctrl->park_lens.max_step) :
 				0;
 		}
+
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+		pr_err("%s next lens_pos %d Line %d\n",
+				__func__,next_lens_pos,__LINE__);
+#endif
 		a_ctrl->func_tbl->actuator_parse_i2c_params(a_ctrl,
 			next_lens_pos, a_ctrl->park_lens.hw_params,
 			a_ctrl->park_lens.damping_delay);

drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -32,7 +32,11 @@
 #define CYCLES_PER_MICRO_SEC_DEFAULT 4915
 #define CCI_MAX_DELAY 1000000
 
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+#define CCI_TIMEOUT msecs_to_jiffies(800)
+#else
 #define CCI_TIMEOUT msecs_to_jiffies(500)
+#endif
 
 /* TODO move this somewhere else */
 #define MSM_CCI_DRV_NAME "msm_cci"

drivers/media/platform/msm/camera_v2/sensor/csid/msm_csid.c
@@ -75,7 +75,11 @@
 #define FALSE  0
 
 #define MAX_LANE_COUNT 4
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+#define CSID_TIMEOUT msecs_to_jiffies(800)
+#else
 #define CSID_TIMEOUT msecs_to_jiffies(100)
+#endif
 
 #undef CDBG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)

drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -10,6 +10,9 @@
  * GNU General Public License for more details.
  */
 #include "msm_sensor.h"
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+#include <linux/gpio.h>
+#endif
 #include "msm_sd.h"
 #include "camera.h"
 #include "msm_cci.h"
@@ -21,6 +24,9 @@
 #undef CDBG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
 
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+extern int lct_hardwareid;
+#endif
 static struct msm_camera_i2c_fn_t msm_sensor_cci_func_tbl;
 static struct msm_camera_i2c_fn_t msm_sensor_secure_func_tbl;
 
@@ -116,6 +122,81 @@ int32_t msm_sensor_free_sensor_data(struct msm_sensor_ctrl_t *s_ctrl)
 	return 0;
 }
 
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+static int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	int rc = 0;
+	uint16_t vendorid = 0;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+	struct msm_camera_slave_info *slave_info;
+	const char *sensor_name;
+	uint16_t temp_sid = 0;
+	enum cci_i2c_master_t temp_master = MASTER_0;
+
+	if (!s_ctrl) {
+		pr_err("%s:%d failed: %pK\n", __func__, __LINE__, s_ctrl);
+		return -EINVAL;
+	}
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	slave_info = s_ctrl->sensordata->slave_info;
+	sensor_name = s_ctrl->sensordata->sensor_name;
+
+	if (!sensor_i2c_client || !slave_info || !sensor_name) {
+		pr_err("%s:%d failed: %pK %pK %pK\n",
+			__func__, __LINE__, sensor_i2c_client, slave_info, sensor_name);
+		return -EINVAL;
+	}
+
+	if (s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr == 0) {
+		pr_err("%s: %s: read 3\n", __func__, sensor_name);
+		return rc;
+	}
+
+	temp_master = sensor_i2c_client->cci_client->cci_i2c_master;
+	switch (s_ctrl->sensordata->vendor_id_info->cci_i2c_master) {
+	case MSM_MASTER_0:
+		sensor_i2c_client->cci_client->cci_i2c_master = MASTER_0;
+		break;
+	case MSM_MASTER_1:
+		sensor_i2c_client->cci_client->cci_i2c_master = MASTER_1;
+		break;
+	default:
+		break;
+	}
+	temp_sid = sensor_i2c_client->cci_client->sid;
+
+	sensor_i2c_client->cci_client->sid =
+		s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr >> 1;
+
+
+	rc = msm_camera_cci_i2c_read(
+		sensor_i2c_client,
+		s_ctrl->sensordata->vendor_id_info->vendor_id_addr,
+		&vendorid,
+		s_ctrl->sensordata->vendor_id_info->data_type);
+
+	sensor_i2c_client->cci_client->sid = temp_sid;
+	sensor_i2c_client->cci_client->cci_i2c_master = temp_master;
+	if (rc < 0) {
+		pr_err("%s: %s: read vendor id failed\n", __func__, sensor_name);
+		return rc;
+	}
+	if (s_ctrl->sensordata->vendor_id_info->vendor_id != vendorid) {
+		pr_err("%s:%s match vendor id failed read vendor id:0x%x expected id 0x%x eeprom_slave_addr 0x%x vendor_id_addr 0x%x\n",
+			__func__,s_ctrl->sensordata->sensor_name,vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id,
+		s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr,
+		s_ctrl->sensordata->vendor_id_info->vendor_id_addr);
+		rc = -1;
+		return rc;
+    }
+	pr_err("%s: read vendor id: 0x%x expected id 0x%x:\n",
+			__func__, vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id);
+
+	return rc;
+}
+#endif
+
 int msm_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 {
 	struct msm_camera_power_ctrl_t *power_info;
@@ -149,6 +230,131 @@ int msm_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 		sensor_i2c_client);
 }
 
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+static int msm_sensor_get_sensor_id_gc02m1(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	int rc = 0;
+	uint16_t vendorid = 0;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	CDBG("%s:%d E \n", __func__, __LINE__);
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+
+	rc = sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfe,
+		0x00, MSM_CAMERA_I2C_BYTE_DATA);
+	if (rc < 0) {
+		pr_err("%s:litao write 0xfe failed\n", __func__);
+		return rc;
+	}
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x01, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf4,
+		0x41, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf5,
+		0xc0, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf6,
+		0x44, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf8,
+		0x38, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf9,
+		0x82, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfa,
+		0x00, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfd,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x81, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfe,
+		0x03, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0x01,
+		0x0b, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf7,
+		0x01, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x8e, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf3,
+		0x30, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfe,
+		0x02, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0x17,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf3,
+		0x34, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_read(
+		sensor_i2c_client,0x19,
+		&vendorid, MSM_CAMERA_I2C_BYTE_DATA);
+
+	if (2 == lct_hardwareid)
+		vendorid = 0x27;
+
+	CDBG("%s:litao read from 0x19 vendorid %d\n", __func__,vendorid);
+
+	if ((!strcmp("ginkgo_gc02m1_sunny_ii", s_ctrl->sensordata->sensor_name)) ||
+	    (!strcmp("ginkgo_gc02m1_ofilm_ii", s_ctrl->sensordata->sensor_name)) || 
+	    (!strcmp("ginkgo_gc02m1_ofilm_iii", s_ctrl->sensordata->sensor_name))) {
+		if (s_ctrl->sensordata->vendor_id_info->vendor_id != vendorid) {
+			pr_err("%s:%s match vendor id failed read vendor id:0x%x expected id 0x%x eeprom_slave_addr 0x%x vendor_id_addr 0x%x\n",
+				__func__,s_ctrl->sensordata->sensor_name,vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id,
+			s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr,
+			s_ctrl->sensordata->vendor_id_info->vendor_id_addr);
+			rc = -1;
+			return rc;
+		}
+	} else {
+		pr_err("litao sensor name failed");
+	}
+	return rc;
+}
+#endif
+
 int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 {
 	int rc;
@@ -157,6 +363,9 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 	struct msm_camera_slave_info *slave_info;
 	const char *sensor_name;
 	uint32_t retry = 0;
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	uint32_t gval;
+#endif
 
 	if (!s_ctrl) {
 		pr_err("%s:%d failed: %pK\n",
@@ -186,7 +395,12 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 	CDBG("Sensor %d tagged as %s\n", s_ctrl->id,
 		(s_ctrl->is_secure)?"SECURE":"NON-SECURE");
 
-	for (retry = 0; retry < 3; retry++) {
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	for (retry = 0; retry < 2; retry++)
+#else
+	for (retry = 0; retry < 3; retry++)
+#endif
+	{
 		if (s_ctrl->is_secure) {
 			rc = msm_camera_tz_i2c_power_up(sensor_i2c_client);
 			if (rc < 0) {
@@ -208,6 +422,48 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 			sensor_i2c_client);
 		if (rc < 0)
 			return rc;
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+		gval = gpio_get_value(132); /* get gpio value!!! */
+
+		CDBG("gpio132 value = %d", gval);
+	    if ((!strcmp("ginkgo_gc02m1_sunny_ii", sensor_name)) || 
+		    (!strcmp("ginkgo_gc02m1_ofilm_ii", sensor_name)) || 
+		    (!strcmp("ginkgo_gc02m1_ofilm_iii", s_ctrl->sensordata->sensor_name))) {
+		    rc = msm_sensor_get_sensor_id_gc02m1(s_ctrl);
+			if (rc < 0) {
+				pr_err("%s:%d litao read sensor %s fusion id failed\n",
+				       __func__, __LINE__, s_ctrl->sensordata->sensor_name);
+				msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+				msleep(20);
+				continue;
+			}	
+		}
+
+		if ((strcmp(sensor_name, "ginkgo_ov02a10_sunny_i") == 0) && (gval == 1)) {
+			msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+			pr_err("%s	probe fail!! ", sensor_name);
+			return -EINVAL;
+		}
+
+		if ((strcmp(sensor_name,"ginkgo_ov02a10_ofilm_ii") == 0) && (gval == 0)) {
+			msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+			pr_err("%s probe fail!! ", sensor_name);
+			return -EINVAL;
+		}
+
+		if ((strcmp(sensor_name, "ginkgo_gc02m1_sunny_ii") != 0) && 
+		    (strcmp(sensor_name,"ginkgo_gc02m1_ofilm_ii") != 0) && 
+		    (strcmp(sensor_name,"ginkgo_gc02m1_ofilm_iii") != 0)) {
+			if (!s_ctrl->is_probe_succeed) {
+				rc = msm_sensor_match_vendor_id(s_ctrl);
+				if (rc < 0) {
+					msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+					msleep(20);
+					continue;
+				}
+			}
+		}
+#endif
 		rc = msm_sensor_check_id(s_ctrl);
 		if (rc < 0) {
 			msm_camera_power_down(power_info,

include/soc/qcom/camera2.h
@@ -153,6 +153,9 @@ struct msm_camera_sensor_board_info {
 	const char *special_support_sensors[MAX_SPECIAL_SUPPORT_SIZE];
 	int32_t special_support_size;
 	struct msm_camera_slave_info *slave_info;
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	struct msm_vendor_id_info_t *vendor_id_info;
+#endif
 	struct msm_camera_csi_lane_params *csi_lane_params;
 	struct msm_camera_sensor_strobe_flash_data *strobe_flash_data;
 	struct msm_actuator_info *actuator_info;

include/uapi/media/msm_camsensor_sdk.h
@@ -61,6 +61,9 @@ enum msm_sensor_camera_id_t {
 	CAMERA_1,
 	CAMERA_2,
 	CAMERA_3,
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	CAMERA_4,
+#endif
 	MAX_CAMERAS,
 };
 
@@ -75,7 +78,14 @@ enum i2c_freq_mode_t {
 enum camb_position_t {
 	BACK_CAMERA_B,
 	FRONT_CAMERA_B,
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	AUX_CAMERA_G_B = 0x110, /* for wide camera */
+	AUX_CAMERA_W_B = 0x120, /* for micro camera */
+#endif
 	AUX_CAMERA_B = 0x100,
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	AUX_CAMERA_FRONT_B = 0x101, /* for front aux camera */
+#endif
 	INVALID_CAMERA_B,
 };
 
@@ -313,6 +323,23 @@ struct msm_sensor_id_info_t {
 	struct msm_camera_i2c_reg_setting setting;
 };
 
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+enum msm_cci_i2c_master_t {
+	MSM_MASTER_0,
+	MSM_MASTER_1,
+	MSM_MASTER_MAX,
+};
+
+struct msm_vendor_id_info_t {
+	unsigned short eeprom_slave_addr;
+	unsigned short vendor_id_addr;
+	enum msm_camera_i2c_reg_addr_type addr_type;
+	unsigned short vendor_id;
+	enum msm_camera_i2c_data_type data_type;
+	enum msm_cci_i2c_master_t cci_i2c_master;
+};
+#endif
+
 struct msm_camera_sensor_slave_info {
 	char sensor_name[32];
 	char eeprom_name[32];
@@ -324,6 +351,9 @@ struct msm_camera_sensor_slave_info {
 	enum i2c_freq_mode_t i2c_freq_mode;
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	struct msm_sensor_id_info_t sensor_id_info;
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	struct msm_vendor_id_info_t vendor_id_info;
+#endif
 	struct msm_sensor_power_setting_array power_setting_array;
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;

LineageOS/android_kernel_xiaomi_sm6125

Change-Id: If71b4eb58f67094a30ddfdd0dff89c06e3b98d6f
Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

drivers/media/platform/msm/camera_v2/isp/msm_isp48.c
@@ -30,7 +30,11 @@
 #define MSM_VFE48_BUS_CLIENT_INIT 0xABAB
 #define VFE48_STATS_BURST_LEN 3
 #define VFE48_UB_SIZE_VFE 2048 /* 2048 * 256 bits = 64KB */
+#ifdef CONFIG_MACH_XIAOMI_C3J
+#define VFE48_UB_STATS_SIZE 608
+#else
 #define VFE48_UB_STATS_SIZE 352
+#endif
 #define MSM_ISP48_TOTAL_IMAGE_UB_VFE (VFE48_UB_SIZE_VFE - VFE48_UB_STATS_SIZE)
 
 
@@ -319,8 +323,23 @@ static void msm_vfe48_set_bus_err_ign_mask(struct vfe_device *vfe_dev,
 void msm_vfe48_stats_cfg_ub(struct vfe_device *vfe_dev)
 {
 	int i;
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	uint32_t ub_offset = 0, stats_burst_len = 0;
+#else
 	uint32_t ub_offset = 0, stats_burst_len;
+#endif
 	uint32_t ub_size[VFE47_NUM_STATS_TYPE] = {
+#ifdef CONFIG_MACH_XIAOMI_C3J
+		80, /* MSM_ISP_STATS_HDR_BE */
+		64, /* MSM_ISP_STATS_BG */
+		64, /* MSM_ISP_STATS_BF */
+		64, /* MSM_ISP_STATS_HDR_BHIST */
+		64, /* MSM_ISP_STATS_RS */
+		64, /* MSM_ISP_STATS_CS */
+		64, /* MSM_ISP_STATS_IHIST */
+		64, /* MSM_ISP_STATS_BHIST */
+		80, /* MSM_ISP_STATS_AEC_BG */
+#else
 		64, /* MSM_ISP_STATS_HDR_BE */
 		64, /* MSM_ISP_STATS_BG */
 		32, /* MSM_ISP_STATS_BF */
@@ -330,6 +349,7 @@ void msm_vfe48_stats_cfg_ub(struct vfe_device *vfe_dev)
 		32, /* MSM_ISP_STATS_IHIST */
 		32, /* MSM_ISP_STATS_BHIST */
 		32, /* MSM_ISP_STATS_AEC_BG */
+#endif
 	};
 
 	stats_burst_len = VFE48_STATS_BURST_LEN;

drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -844,6 +844,10 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 		a_ctrl->park_lens.max_step = a_ctrl->max_code_size;
 
 	next_lens_pos = a_ctrl->step_position_table[a_ctrl->curr_step_pos];
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	if (next_lens_pos > 100)
+		next_lens_pos = 100;
+#endif
 	while (next_lens_pos) {
 		/* conditions which help to reduce park lens time */
 		if (next_lens_pos > (a_ctrl->park_lens.max_step *
@@ -867,6 +871,11 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 				(next_lens_pos - a_ctrl->park_lens.max_step) :
 				0;
 		}
+
+#ifdef CONFIG_MACH_XIAOMI_C3J
+		pr_err("%s next lens_pos %d Line %d\n",
+				__func__,next_lens_pos,__LINE__);
+#endif
 		a_ctrl->func_tbl->actuator_parse_i2c_params(a_ctrl,
 			next_lens_pos, a_ctrl->park_lens.hw_params,
 			a_ctrl->park_lens.damping_delay);

drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -35,8 +35,9 @@
 
 #ifdef CONFIG_MACH_XIAOMI_F9S
 #define CCI_TIMEOUT msecs_to_jiffies(1000)
-#else
-#define CCI_TIMEOUT msecs_to_jiffies(500)
+#endif
+#ifdef CONFIG_MACH_XIAOMI_C3J
+#define CCI_TIMEOUT msecs_to_jiffies(800)
 #endif
 
 /* TODO move this somewhere else */

drivers/media/platform/msm/camera_v2/sensor/csid/msm_csid.c
@@ -75,7 +75,11 @@
 #define FALSE  0
 
 #define MAX_LANE_COUNT 4
+#ifdef CONFIG_MACH_XIAOMI_C3J
+#define CSID_TIMEOUT msecs_to_jiffies(800)
+#else
 #define CSID_TIMEOUT msecs_to_jiffies(100)
+#endif
 
 #undef CDBG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)

drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -10,6 +10,9 @@
  * GNU General Public License for more details.
  */
 #include "msm_sensor.h"
+#ifdef CONFIG_MACH_XIAOMI_C3J
+#include <linux/gpio.h>
+#endif
 #include "msm_sd.h"
 #include "camera.h"
 #include "msm_cci.h"
@@ -21,6 +24,9 @@
 #undef CDBG
 #define CDBG(fmt, args...) pr_debug(fmt, ##args)
 
+#ifdef CONFIG_MACH_XIAOMI_C3J
+extern int lct_hardwareid;
+#endif
 static struct msm_camera_i2c_fn_t msm_sensor_cci_func_tbl;
 static struct msm_camera_i2c_fn_t msm_sensor_secure_func_tbl;
 
@@ -116,6 +122,81 @@ int32_t msm_sensor_free_sensor_data(struct msm_sensor_ctrl_t *s_ctrl)
 	return 0;
 }
 
+#ifdef CONFIG_MACH_XIAOMI_C3J
+static int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	int rc = 0;
+	uint16_t vendorid = 0;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+	struct msm_camera_slave_info *slave_info;
+	const char *sensor_name;
+	uint16_t temp_sid = 0;
+	enum cci_i2c_master_t temp_master = MASTER_0;
+
+	if (!s_ctrl) {
+		pr_err("%s:%d failed: %pK\n", __func__, __LINE__, s_ctrl);
+		return -EINVAL;
+	}
+
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+	slave_info = s_ctrl->sensordata->slave_info;
+	sensor_name = s_ctrl->sensordata->sensor_name;
+
+	if (!sensor_i2c_client || !slave_info || !sensor_name) {
+		pr_err("%s:%d failed: %pK %pK %pK\n",
+			__func__, __LINE__, sensor_i2c_client, slave_info, sensor_name);
+		return -EINVAL;
+	}
+
+	if (s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr == 0) {
+		pr_err("%s: %s: read 3\n", __func__, sensor_name);
+		return rc;
+	}
+
+	temp_master = sensor_i2c_client->cci_client->cci_i2c_master;
+	switch (s_ctrl->sensordata->vendor_id_info->cci_i2c_master) {
+	case MSM_MASTER_0:
+		sensor_i2c_client->cci_client->cci_i2c_master = MASTER_0;
+		break;
+	case MSM_MASTER_1:
+		sensor_i2c_client->cci_client->cci_i2c_master = MASTER_1;
+		break;
+	default:
+		break;
+	}
+	temp_sid = sensor_i2c_client->cci_client->sid;
+
+	sensor_i2c_client->cci_client->sid =
+		s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr >> 1;
+
+
+	rc = msm_camera_cci_i2c_read(
+		sensor_i2c_client,
+		s_ctrl->sensordata->vendor_id_info->vendor_id_addr,
+		&vendorid,
+		s_ctrl->sensordata->vendor_id_info->data_type);
+
+	sensor_i2c_client->cci_client->sid = temp_sid;
+	sensor_i2c_client->cci_client->cci_i2c_master = temp_master;
+	if (rc < 0) {
+		pr_err("%s: %s: read vendor id failed\n", __func__, sensor_name);
+		return rc;
+	}
+	if (s_ctrl->sensordata->vendor_id_info->vendor_id != vendorid) {
+		pr_err("%s:%s match vendor id failed read vendor id:0x%x expected id 0x%x eeprom_slave_addr 0x%x vendor_id_addr 0x%x\n",
+			__func__,s_ctrl->sensordata->sensor_name,vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id,
+		s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr,
+		s_ctrl->sensordata->vendor_id_info->vendor_id_addr);
+		rc = -1;
+		return rc;
+    }
+	pr_err("%s: read vendor id: 0x%x expected id 0x%x:\n",
+			__func__, vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id);
+
+	return rc;
+}
+#endif
+
 int msm_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 {
 	struct msm_camera_power_ctrl_t *power_info;
@@ -149,6 +230,131 @@ int msm_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 		sensor_i2c_client);
 }
 
+#ifdef CONFIG_MACH_XIAOMI_C3J
+static int msm_sensor_get_sensor_id_gc02m1(struct msm_sensor_ctrl_t *s_ctrl)
+{
+	int rc = 0;
+	uint16_t vendorid = 0;
+	struct msm_camera_i2c_client *sensor_i2c_client;
+
+	CDBG("%s:%d E \n", __func__, __LINE__);
+	sensor_i2c_client = s_ctrl->sensor_i2c_client;
+
+	rc = sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfe,
+		0x00, MSM_CAMERA_I2C_BYTE_DATA);
+	if (rc < 0) {
+		pr_err("%s:litao write 0xfe failed\n", __func__);
+		return rc;
+	}
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x01, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf4,
+		0x41, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf5,
+		0xc0, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf6,
+		0x44, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf8,
+		0x38, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf9,
+		0x82, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfa,
+		0x00, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfd,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x81, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfe,
+		0x03, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0x01,
+		0x0b, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf7,
+		0x01, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfc,
+		0x8e, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf3,
+		0x30, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xfe,
+		0x02, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0x17,
+		0x80, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_write(
+		sensor_i2c_client, 0xf3,
+		0x34, MSM_CAMERA_I2C_BYTE_DATA);
+
+	sensor_i2c_client->i2c_func_tbl->i2c_read(
+		sensor_i2c_client,0x19,
+		&vendorid, MSM_CAMERA_I2C_BYTE_DATA);
+
+	if (2 == lct_hardwareid)
+		vendorid = 0x27;
+
+	CDBG("%s:litao read from 0x19 vendorid %d\n", __func__,vendorid);
+
+	if ((!strcmp("ginkgo_gc02m1_sunny_ii", s_ctrl->sensordata->sensor_name)) ||
+	    (!strcmp("ginkgo_gc02m1_ofilm_ii", s_ctrl->sensordata->sensor_name)) || 
+	    (!strcmp("ginkgo_gc02m1_ofilm_iii", s_ctrl->sensordata->sensor_name))) {
+		if (s_ctrl->sensordata->vendor_id_info->vendor_id != vendorid) {
+			pr_err("%s:%s match vendor id failed read vendor id:0x%x expected id 0x%x eeprom_slave_addr 0x%x vendor_id_addr 0x%x\n",
+				__func__,s_ctrl->sensordata->sensor_name,vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id,
+			s_ctrl->sensordata->vendor_id_info->eeprom_slave_addr,
+			s_ctrl->sensordata->vendor_id_info->vendor_id_addr);
+			rc = -1;
+			return rc;
+		}
+	} else {
+		pr_err("litao sensor name failed");
+	}
+	return rc;
+}
+#endif
+
 int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 {
 	int rc;
@@ -157,6 +363,9 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 	struct msm_camera_slave_info *slave_info;
 	const char *sensor_name;
 	uint32_t retry = 0;
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	uint32_t gval;
+#endif
 
 	if (!s_ctrl) {
 		pr_err("%s:%d failed: %pK\n",
@@ -186,7 +395,12 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 	CDBG("Sensor %d tagged as %s\n", s_ctrl->id,
 		(s_ctrl->is_secure)?"SECURE":"NON-SECURE");
 
-	for (retry = 0; retry < 3; retry++) {
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	for (retry = 0; retry < 2; retry++)
+#else
+	for (retry = 0; retry < 3; retry++)
+#endif
+	{
 		if (s_ctrl->is_secure) {
 			rc = msm_camera_tz_i2c_power_up(sensor_i2c_client);
 			if (rc < 0) {
@@ -208,6 +422,48 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 			sensor_i2c_client);
 		if (rc < 0)
 			return rc;
+#ifdef CONFIG_MACH_XIAOMI_C3J
+		gval = gpio_get_value(132); /* get gpio value!!! */
+
+		CDBG("gpio132 value = %d", gval);
+	    if ((!strcmp("ginkgo_gc02m1_sunny_ii", sensor_name)) || 
+		    (!strcmp("ginkgo_gc02m1_ofilm_ii", sensor_name)) || 
+		    (!strcmp("ginkgo_gc02m1_ofilm_iii", s_ctrl->sensordata->sensor_name))) {
+		    rc = msm_sensor_get_sensor_id_gc02m1(s_ctrl);
+			if (rc < 0) {
+				pr_err("%s:%d litao read sensor %s fusion id failed\n",
+				       __func__, __LINE__, s_ctrl->sensordata->sensor_name);
+				msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+				msleep(20);
+				continue;
+			}	
+		}
+
+		if ((strcmp(sensor_name, "ginkgo_ov02a10_sunny_i") == 0) && (gval == 1)) {
+			msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+			pr_err("%s	probe fail!! ", sensor_name);
+			return -EINVAL;
+		}
+
+		if ((strcmp(sensor_name,"ginkgo_ov02a10_ofilm_ii") == 0) && (gval == 0)) {
+			msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+			pr_err("%s probe fail!! ", sensor_name);
+			return -EINVAL;
+		}
+
+		if ((strcmp(sensor_name, "ginkgo_gc02m1_sunny_ii") != 0) && 
+		    (strcmp(sensor_name,"ginkgo_gc02m1_ofilm_ii") != 0) && 
+		    (strcmp(sensor_name,"ginkgo_gc02m1_ofilm_iii") != 0)) {
+			if (!s_ctrl->is_probe_succeed) {
+				rc = msm_sensor_match_vendor_id(s_ctrl);
+				if (rc < 0) {
+					msm_camera_power_down(power_info, s_ctrl->sensor_device_type, sensor_i2c_client);
+					msleep(20);
+					continue;
+				}
+			}
+		}
+#endif
 		rc = msm_sensor_check_id(s_ctrl);
 		if (rc < 0) {
 			msm_camera_power_down(power_info,

include/soc/qcom/camera2.h
@@ -153,6 +153,9 @@ struct msm_camera_sensor_board_info {
 	const char *special_support_sensors[MAX_SPECIAL_SUPPORT_SIZE];
 	int32_t special_support_size;
 	struct msm_camera_slave_info *slave_info;
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	struct msm_vendor_id_info_t *vendor_id_info;
+#endif
 	struct msm_camera_csi_lane_params *csi_lane_params;
 	struct msm_camera_sensor_strobe_flash_data *strobe_flash_data;
 	struct msm_actuator_info *actuator_info;

include/uapi/media/msm_camsensor_sdk.h
@@ -77,7 +77,14 @@ enum i2c_freq_mode_t {
 enum camb_position_t {
 	BACK_CAMERA_B,
 	FRONT_CAMERA_B,
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	AUX_CAMERA_G_B = 0x110, /* for wide camera */
+	AUX_CAMERA_W_B = 0x120, /* for micro camera */
+#endif
 	AUX_CAMERA_B = 0x100,
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	AUX_CAMERA_FRONT_B = 0x101, /* for front aux camera */
+#endif
 	INVALID_CAMERA_B,
 };
 
@@ -318,6 +325,23 @@ struct msm_sensor_id_info_t {
 	struct msm_camera_i2c_reg_setting setting;
 };
 
+#ifdef CONFIG_MACH_XIAOMI_C3J
+enum msm_cci_i2c_master_t {
+	MSM_MASTER_0,
+	MSM_MASTER_1,
+	MSM_MASTER_MAX,
+};
+
+struct msm_vendor_id_info_t {
+	unsigned short eeprom_slave_addr;
+	unsigned short vendor_id_addr;
+	enum msm_camera_i2c_reg_addr_type addr_type;
+	unsigned short vendor_id;
+	enum msm_camera_i2c_data_type data_type;
+	enum msm_cci_i2c_master_t cci_i2c_master;
+};
+#endif
+
 struct msm_camera_sensor_slave_info {
 	char sensor_name[32];
 	char eeprom_name[32];
@@ -329,6 +353,9 @@ struct msm_camera_sensor_slave_info {
 	enum i2c_freq_mode_t i2c_freq_mode;
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	struct msm_sensor_id_info_t sensor_id_info;
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	struct msm_vendor_id_info_t vendor_id_info;
+#endif
 	struct msm_sensor_power_setting_array power_setting_array;
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;